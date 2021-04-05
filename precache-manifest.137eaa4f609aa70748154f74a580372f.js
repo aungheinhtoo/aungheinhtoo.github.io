59ffaf32a064f8d168dab09a6ac5503f5ed619cf
aungheinhtoo <44188603+aungheinhtoo@users.noreply.github.com>
2021-04-06 01:00:06 +0800

    Updates

---
+++ precache-manifest.137eaa4f609aa70748154f74a580372f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f2809b0849b7755c88f95c8da3d30f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "92438fc7e7a8ed5f1978",
+    "url": "/portfolio/static/css/main.c68c6e4d.chunk.css"
+  },
+  {
+    "revision": "f44acd7151fee9183453",
+    "url": "/portfolio/static/js/2.bfc4e68a.chunk.js"
+  },
+  {
+    "revision": "92438fc7e7a8ed5f1978",
+    "url": "/portfolio/static/js/main.b5a3cb9d.chunk.js"
+  },
+  {
+    "revision": "87b8676f3a5f2a732810",
+    "url": "/portfolio/static/js/runtime-main.fc8ce58b.js"
+  }
+]);
